5ac1ad9505cee6fb1d636fb83502b261136a31d7
Damien Mayance <dmayance@gmail.com>
2013-11-12 17:53:41 +0100

    Part3 - Player and enemies

---
+++ source/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Simply moves the current game object
+/// </summary>
+public class MoveScript : MonoBehaviour
+{
+  // 0 - Designer variables
+
+  /// <summary>
+  /// Projectile speed
+  /// </summary>
+  public Vector2 Speed = new Vector2(10, 10);
+
+  /// <summary>
+  /// Moving direction
+  /// </summary>
+  public Vector2 Direction = new Vector2(-1, 0);
+
+  void Update()
+  {
+    // 1 - Movement
+    Vector3 movement = new Vector3(
+      Speed.x * Direction.x,
+      Speed.y * Direction.y,
+      0);
+
+    movement *= Time.deltaTime;
+    transform.Translate(movement);
+  }
+}
---
+++ source/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+  /// <summary>
+  /// 0 - The speed of the ship
+  /// </summary>
+  public Vector2 Speed = new Vector2(25, 25);
+
+  void Update()
+  {
+    // 1 - Retrieve axis information
+    float inputX = Input.GetAxis("Horizontal");
+    float inputY = Input.GetAxis("Vertical");
+
+    // 2 - Movement per direction
+    Vector3 movement = new Vector3(
+      Speed.x * inputX,
+      Speed.y * inputY,
+      0);
+
+    // 3 - Relative to the time
+    movement *= Time.deltaTime;
+
+    // 4 - Move the game object
+    transform.Translate(movement);
+  }
+}
