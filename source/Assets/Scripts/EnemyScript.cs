d9aa28d63a58485d1275523c879f6c476ce2e279
Damien Mayance <dmayance@gmail.com>
2013-11-12 17:54:20 +0100

    Part5 - Shooting 2/2

---
+++ source/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enemy generic behavior
+/// </summary>
+public class EnemyScript : MonoBehaviour
+{
+  private WeaponScript[] weapons;
+
+  void Awake()
+  {
+    // Retrieve the weapon only once
+    weapons = GetComponentsInChildren<WeaponScript>();
+  }
+
+  void Update()
+  {
+    foreach (WeaponScript weapon in weapons)
+    {
+      // Auto-fire
+      if (weapon != null && weapon.CanAttack)
+      {
+        weapon.Attack(true);
+      }
+    }
+  }
+}
