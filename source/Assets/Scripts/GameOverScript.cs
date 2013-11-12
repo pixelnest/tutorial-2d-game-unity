6e7ae48511204be8c3eee2e74c273dc90f80f674
Damien Mayance <dmayance@gmail.com>
2013-11-12 19:00:12 +0100

    Menu with image as button

--- source/Assets/Scripts/GameOverScript.cs
+++ source/Assets/Scripts/GameOverScript.cs
@@ -5,15 +5,24 @@
 /// </summary>
 public class GameOverScript : MonoBehaviour
 {
+  private GUISkin skin;
+
+  void Start()
+  {
+    skin = Resources.Load("GUISkin") as GUISkin;
+  }
+
   void OnGUI()
   {
-    const int buttonWidth = 120;
+    const int buttonWidth = 140;
     const int buttonHeight = 60;
+
+    GUI.skin = skin;
 
     if (GUI.Button(
       // Center in X, 1/3 of the height in Y
       new Rect(Screen.width / 2 - (buttonWidth / 2), (1 * Screen.height / 3) - (buttonHeight / 2), buttonWidth, buttonHeight),
-      "Retry!"
+      "RETRY"
       ))
     {
       // Reload the level
@@ -23,7 +32,7 @@ public class GameOverScript : MonoBehaviour
     if (GUI.Button(
       // Center in X, 2/3 of the height in Y
       new Rect(Screen.width / 2 - (buttonWidth / 2), (2 * Screen.height / 3) - (buttonHeight / 2), buttonWidth, buttonHeight),
-      "Back to menu"
+      "BACK"
       ))
     {
       // Reload the level
--- source/Assets/Scripts/MenuScript.cs
+++ source/Assets/Scripts/MenuScript.cs
@@ -5,16 +5,25 @@
 /// </summary>
 public class MenuScript : MonoBehaviour
 {
+  private GUISkin skin;
+
+  void Start()
+  {
+    skin = Resources.Load("GUISkin") as GUISkin;
+  }
+
   void OnGUI()
   {
-    const int buttonWidth = 84;
+    const int buttonWidth = 128;
     const int buttonHeight = 60;
 
+    GUI.skin = skin;
+    Debug.Log("skin: " + skin);
     // Draw a button to start the game
     if (GUI.Button(
       // Center in X, 2/3 of the height in Y
       new Rect(Screen.width / 2 - (buttonWidth / 2), (2 * Screen.height / 3) - (buttonHeight / 2), buttonWidth, buttonHeight),
-      "Start!"
+      "START"
       ))
     {
       // On Click, load the first level.
